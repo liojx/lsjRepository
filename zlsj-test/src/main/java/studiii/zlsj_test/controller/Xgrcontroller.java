14a218433416eaec59711bf8ba5230cdfe46648c
liaodaye <liao_2464@163.com>
2018-08-17 15:46:22 +0800

    提交T30

--- zlsj-test/src/main/java/studiii/zlsj_test/controller/Xgrcontroller.java
+++ zlsj-test/src/main/java/studiii/zlsj_test/controller/Xgrcontroller.java
@@ -1,6 +1,7 @@
 package studiii.zlsj_test.controller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -9,5 +10,16 @@ public class Xgrcontroller {
 	@RequestMapping("/T25")
 	void prit() {
 		System.out.println("Now ,i am bigger...");
+	}
+
+	@RequestMapping("/T29")
+	@ResponseBody
+	public String getSome(){
+		return  "some one is coming...";
+	}
+	
+	@RequestMapping("/T30")
+	String getOnePeaceOFCake(){
+		return "tomcat navicat";
 	}
 }
